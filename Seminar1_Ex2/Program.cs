d03a2d226f2e24915ed60bca399d67e4c48611c7
Kristina <kkashkareva@mail.ru>
2022-11-18 23:52:31 +0400

    Seminar1_ex2

---
+++ Seminar1_Ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Seminar1_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int numberA = 2;
+            int numberB = 4;
+            if (numberB == numberA * numberA)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+        }   
+        
+            
+        
+            
+        
+    }
+}
