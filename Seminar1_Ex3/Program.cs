7c8bf7beae7e06cf0f8ceaf463b23376557f7472
Kristina <kkashkareva@mail.ru>
2022-11-18 23:49:39 +0400

    Seminar1_ex3

---
+++ Seminar1_Ex3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Seminar1_Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber = new int();
+            Console.Write("Введите номер дня недели");
+            userNumber = Convert.ToInt32(Console.ReadLine());
+            if(userNumber == 1)
+            {
+                Console.WriteLine("Понедельник");
+            }
+            else if (userNumber == 2)
+            {
+                Console.WriteLine("Вторник");
+            }
+            else if (userNumber == 3)
+            {
+                Console.WriteLine("Среда");
+            }
+            else if (userNumber == 4)
+            {
+                Console.WriteLine("Четверг");
+            }
+            else if (userNumber == 5)
+            {
+               Console.WriteLine("Пятница"); 
+            }
+            else if (userNumber == 6)
+            {
+               Console.WriteLine("Суббота");  
+            }
+            else if (userNumber == 7)
+            {
+                Console.WriteLine("Воскресенье"); 
+            }
+            else
+            {
+                Console.WriteLine("Такого дня недели не существует!");
+            }
+        }   
+    }
+}
