06f231acd1d8741347a11e24ce4800b0f3f1a422
Kristina <kkashkareva@mail.ru>
2022-11-18 21:59:39 +0400

    Seminar1_ex1

---
+++ Seminar1_Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example006_HelloMasha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int userNumber = new int();
+         Console.WriteLine("Ведите число");
+         userNumber = Convert.ToInt32(Console.ReadLine());
+         int result = userNumber * userNumber;
+         Console.WriteLine(result);
+        }
+    }
+}
