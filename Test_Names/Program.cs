1a53a10cdaf23fc9d58da592c053b68887dcb9b0
Kristina <kkashkareva@mail.ru>
2022-11-19 18:49:39 +0400

    Объединение строк

---
+++ Test_Names/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объединение строк
+            
+            Console.Write("Введите имя пользователя:");
+            string username = Console.ReadLine();
+            string s3 = "Hello, ";
+            string s4 = s3 + username;
+            Console.WriteLine(s4);
+        }
+    }
+}
