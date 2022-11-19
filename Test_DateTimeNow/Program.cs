9610488215ef2120e29ce6222fe72b9f154ea6f4
Kristina <kkashkareva@mail.ru>
2022-11-19 18:31:02 +0400

    добавлен DateTime Now

---
+++ Test_DateTimeNow/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Вывести текущую дату
+            */
+            
+            Console.Write("Текущая дата:");
+            Console.Write(DateTime.Now);
+        }
+    }
+}
