138c28199d623cb20712314c6c31e5cca457b9b6
Kristina <kkashkareva@mail.ru>
2022-11-20 23:15:38 +0400

    добавлен комментарий к Example004_Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -6,6 +6,8 @@ namespace Example004_Div
     {
         static void Main(string[] args)
         {
+            /* Получение частного при делении двух чисел*/
+            
             double numberA = 12;
             double numberB = 5;
             Console.WriteLine(numberA / numberB);
