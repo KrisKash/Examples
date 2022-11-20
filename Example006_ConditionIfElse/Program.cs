a99e3dc606e286d746416d37f61bd01c4193a97e
Kristina <kkashkareva@mail.ru>
2022-11-20 23:44:11 +0400

    Example006_ConditionIfElse

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example006_ConditionIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Поприветствовать пользователя - любимчика(ветвление)*/
+
+            Console.WriteLine("Введите имя пользователя:");
+            string username = Console.ReadLine();
+
+            if(username.ToLower() == "маша")
+            {
+                Console.WriteLine("Ура, это же Маша!");
+            }
+            else
+            {
+                Console.Write("Привет,");
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
