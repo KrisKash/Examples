9985204fd587ebb5edfda60f892dea0c65ae2023
Kristina <kkashkareva@mail.ru>
2022-11-20 23:12:27 +0400

    добавлен комментарий к Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -6,6 +6,9 @@ namespace Example003_Sum
     {
         static void Main(string[] args)
         {
+            /* Написать программу, в которую пользователь вводит два целых числа 
+            и которая выводит сумму этих чисел*/
+                
             int numberA = 31;
             int numberB = 5;
             int result = numberA + numberB;
