93496ff522b0c12d5e4abbd188bc202337e74a2d
Kristina <kkashkareva@mail.ru>
2022-11-20 23:22:23 +0400

    добавлен комментарий к Example005_Sum1

--- Example005_Sum1/Program.cs
+++ Example005_Sum1/Program.cs
@@ -6,6 +6,7 @@ namespace Example005_Sum1
     {
         static void Main(string[] args)
         {
+            /* Написать программу, которая выводит сумму двух случайных чисел из диапазона (min,max)*/
             int numberA = new Random().Next(1,10); // 1 2 3....9
             Console.WriteLine(numberA);
             int numberB = new Random().Next(1,10);
