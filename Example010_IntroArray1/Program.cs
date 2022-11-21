b54565e8f91ab0671ed70498246c8e9847a70847
Kristina <kkashkareva@mail.ru>
2022-11-21 17:14:01 +0400

    Example010_IntroArray1

---
+++ Example010_IntroArray1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Example010_IntroArray1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+            int n = array.Length; //array.Length - длина/количество символов массива
+            int find = 18; //число, которое может ввести пользователь
+
+            int index = 0;
+
+            while (index < n)
+            {
+                if(array[index] == find)
+                {
+                    Console.WriteLine(index);
+                    break; // завершение работы при нахождении первого подходящего значения
+                }
+                index++; //index = index + 1;
+            }
+
+        }
+    }
+}
