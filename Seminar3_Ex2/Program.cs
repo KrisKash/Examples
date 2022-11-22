19651b646fa1a7595780f2ea6278562a939ec1e7
Kristina <kkashkareva@mail.ru>
2022-11-22 23:03:40 +0400

    Семинар 3

---
+++ Seminar3_Ex2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Seminar3_Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*
+            8 Напишите программу, которая по заданному номеру четверти, 
+            показывает диапазон возможных координат точек в этой четверти (x и y).
+           */ 
+            /*int quater = new int();
+            Console.WriteLine("Введите номер четверти: ");
+            quater = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{quater}");
+            Console.WriteLine();
+            */
+
+
+            int GetCoordinateFromQuater(int quater)
+            {
+                if (quater == 1)
+                {
+                    Console.WriteLine("x > 0 и y > 0");
+                }
+                else if (quater == 2)
+                {
+                    Console.WriteLine("x < 0 и y > 0");
+                }
+                else if (quater == 3)
+                {
+                    Console.WriteLine("x < 0 и y < 0");
+                }
+                else if (quater == 4)
+                {
+                    Console.WriteLine("x > 0 и y < 0");
+                }
+                return quater; 
+            }
+
+            int getNumberFromUser(string message)
+            {
+                int result = 0;
+                Console.WriteLine(message);
+                result = Convert.ToInt32(Console.ReadLine());
+                return result;
+            }
+            int quater = getNumberFromUser("Введите четверть: ");
+            GetCoordinateFromQuater(quater); 
+
+
+
+        }
+    }
+}
---
+++ Seminar3_Ex3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Internal;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text.RegularExpressions;
+using System;
+
+namespace Seminar3_Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*
+           Напишите программу, которая принимает на вход координаты двух точек 
+           и находит расстояние между ними в 2D пространстве.
+           A (3,6); B (2,1) -> 5,09
+           A (7,-5); B (1,-1) -> 7,21
+           */ 
+
+            int getNumberFromUser(string message)
+            {
+            int result = 0;
+            Console.WriteLine(message);
+            result = Convert.ToInt32(Console.ReadLine());
+            return result;
+            }
+
+            double GetDistance (int x1, int y1, int x2, int y2)
+            {
+               double distance = Match.Sqrt(Math.Pow (x2 - x1,2)+ Math.Pow(y2-y1,2));
+            }
+            int x1 = getNumberFromUser("Введите X1:");
+            int y1 = getNumberFromUser("Введите Y1:");
+            int x2 = getNumberFromUser("Введите X2:");
+            int y2 = getNumberFromUser("Введите Y2:");
+            int distance = GetDistance (int x1, int y1, int x2, int y2);
+            Console.WriteLine($"Расстояние между точками равно {distance} ");
+            
+
+    }
+}
