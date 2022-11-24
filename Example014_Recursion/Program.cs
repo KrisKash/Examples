744856021e8ebf67bf9c67a1a8a67d67e4fa4f00
Kristina <kkashkareva@mail.ru>
2022-11-24 17:40:25 +0400

    Рекурсия, задача с факториалом

---
+++ Example014_Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Example014_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Factorial (int n)
+            {
+               // 1! = 1
+               // 0! = 1 
+               if(n == 1 ) return 1;
+               else return n * Factorial(n - 1);
+            }
+            for (int i = 1; i < 40; i++)
+            {
+                Console.WriteLine($"{i}! = {Factorial(i)}");
+            }
+            
+            
+        }
+    }
+}
