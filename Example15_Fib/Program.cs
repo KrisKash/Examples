c1c343751e05498194a19dda04174c6a5ad9ad24
Kristina <kkashkareva@mail.ru>
2022-11-24 18:04:03 +0400

    Рекурсия, задача с числами Фибоначчи

---
+++ Example15_Fib/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Example15_Fib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // f(1) = 1
+            // f(2) = 1
+            // f(n) = f(n-1) + f(n-2)
+
+            double Fibonacci (int n)
+            {
+                if (n == 1 || n == 2) return 1;
+                else
+                {
+                    return Fibonacci(n-1) + Fibonacci(n-2);
+                }
+            }
+            for (int i = 1; i < 50; i++)
+            {
+                Console.WriteLine($"f({i}) = {Fibonacci(i)} ");
+                
+            }
+        }
+    }
+}
