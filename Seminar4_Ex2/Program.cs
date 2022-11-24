b8b1d4a0e5397826d37878b9ad83bd81de9408f5
Kristina <kkashkareva@mail.ru>
2022-11-24 21:53:00 +0400

    задача 26 семинара 4

--- Seminar4_Ex2/Program.cs
+++ Seminar4_Ex2/Program.cs
@@ -6,7 +6,39 @@ namespace Seminar4_Ex2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+            Задача 26: Напишите программу, которая принимает на вход число 
+            и выдаёт количество цифр в числе.
+            456 -> 3
+            78 -> 2
+            89126 -> 5
+            */
+
+            int GetNumberOfDigits(int number)
+            {
+                int numberOfDigit = 0;
+                while (number > 0)
+                {
+                    number /= 10; // number = number / 10
+                    numberOfDigit++;
+                    Console.WriteLine(number);
+                }
+                return numberOfDigit;
+            }
+
+            int getNumberFromUser(string message)
+            {
+                int result = 0;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(message);
+                Console.ResetColor();
+                result = Convert.ToInt32(Console.ReadLine());
+                return result;
+            }
+
+            int userNumber = getNumberFromUser("Введите число:");
+            int numberOfDigit = GetNumberOfDigits(userNumber);
+            Console.WriteLine($"Количество цифр в числе {userNumber} = {numberOfDigit} ");
         }
     }
 }
