7ce4d09cd0b15300437dd5ccb444e2f57f694399
Kristina <kkashkareva@mail.ru>
2022-11-28 23:10:15 +0400

    Задача 42

--- Example15_Fib/Program.cs
+++ Example15_Fib/Program.cs
@@ -10,7 +10,7 @@ namespace Example15_Fib
             // f(2) = 1
             // f(n) = f(n-1) + f(n-2)
 
-            double Fibonacci (int n)
+            /*double Fibonacci (int n)
             {
                 if (n == 1 || n == 2) return 1;
                 else
@@ -23,6 +23,23 @@ namespace Example15_Fib
                 Console.WriteLine($"f({i}) = {Fibonacci(i)} ");
                 
             }
+            */
+            double Fibonacci (int n)
+            {
+                if (n == 1 || n == 2) return 1;
+                else
+                {
+                    
+                    
+                    return Fibonacci(n-1) + Fibonacci(n-2);
+                }
+            }
+            for (int i = 1; i < 50; i++)
+            {
+                Console.WriteLine($"f({i}) = {Fibonacci(i)} ");
+                
+            }
+
         }
     }
 }
---
+++ Seminar6_Ex42/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Seminar6_Ex44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getNumberFromUser(string message) 
+            { 
+                int result = 0; 
+                Console.WriteLine(message); 
+                result = Convert.ToInt32(Console.ReadLine()); 
+                return result; 
+            } 
+            
+            string binaryNumber(int decimalNumber) 
+            { 
+                var binaryNumber = string.Empty; 
+                while (decimalNumber > 0)  
+                { 
+                    binaryNumber = ((decimalNumber % 2) + binaryNumber); 
+                    decimalNumber /= 2; 
+                } 
+                return binaryNumber; 
+            } 
+            
+            int userNumber = getNumberFromUser("Введите число"); 
+            var binNumber = binaryNumber(userNumber); 
+            Console.WriteLine(binNumber);
+        }
+    }
+}
