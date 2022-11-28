3db8a114d7a56828a479e0f521753461390a1a97
Kristina <kkashkareva@mail.ru>
2022-11-28 23:18:44 +0400

    Задача 44

---
+++ Seminar6_Ex44/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Seminar6_Ex44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+            Первые два числа Фибоначчи: 0 и 1.
+            Если N = 5 -> 0 1 1 2 3
+            Если N = 3 -> 0 1 1
+            Если N = 7 -> 0 1 1 2 3 5 8
+            */
+            Console.WriteLine("Введите число");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            int[] fibArray = fillingArray(userNumber);
+            printArray(fibArray);
+
+
+            int[] fillingArray(int number)
+            {
+                int[] fibonachiArray = new int[number];
+                fibonachiArray[0] = 0;
+                fibonachiArray[1] = 1;
+                for (int i = 2; i < number; i++)
+                {
+                    fibonachiArray[i] = fibonachiArray[i - 2] + fibonachiArray[i - 1];
+                }
+                return fibonachiArray;
+            }
+
+            void printArray(int[] array)
+            {
+                Console.Write("[");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1) 
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine("]");
+            }
+        }
+    }
+}
