39a39e2dfefb55341cd124c87729d8489aaed081
Kristina <kkashkareva@mail.ru>
2022-12-01 22:03:13 +0400

    Задача 55

---
+++ Seminar8_Ex55/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seminar8_Ex55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Seminar8_Ex57/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seminar8_Ex57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
