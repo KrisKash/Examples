61fb4c415824b0d8992b55eaa5eb90b96604aa4d
Kristina <kkashkareva@mail.ru>
2022-12-04 12:16:38 +0400

    Задача 63

---
+++ Seminar9_63/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Seminar9_63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Задача 63: Задайте значение N. Напишите программу, которая выведет все 
+            натуральные числа в промежутке от 1 до N.
+            N = 5 -> "1, 2, 3, 4, 5"
+            N = 6 -> "1, 2, 3, 4, 5, 6"
+            */
+            int GetUserInt(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine (message);
+                Console.ResetColor();
+                int userInt = Convert.ToInt32(Console.ReadLine());
+                return userInt;
+            }
+            int startIndex = GetUserInt ("Введите максимальный элемент ряда: ");
+            Console.WriteLine();
+            PrintRange (startIndex);
+            Console.WriteLine();
+
+            void PrintRange (int index)
+            {
+                if (index != 0)
+                {
+                    PrintRange(index-1);
+                    Console.Write(index + ", ");
+                }
+            }
+
+        }
+    }
+}
---
+++ Seminar9_67/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seminar9_67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
